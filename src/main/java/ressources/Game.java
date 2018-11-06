c3728787e81581427e5ce4b57db511bd32bca366
Juliette de Bressieux <jdb@wemanity.com>
2018-11-06 12:03:45 +0100

    Add methods roll and score

---
+++ src/main/java/ressources/Game.java
@@ -0,0 +1,14 @@
+ackage main.java.ressources;
+
+public class Game {
+
+    // roll: called each time the player rolls the ball.
+    public void roll(int numberOfPinsKnockedDown) {
+    }
+
+    public int score() {
+        int score=0;
+        return score;
+    }
+
+}
