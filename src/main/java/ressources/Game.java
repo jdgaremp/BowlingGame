5f7493ef4a995ec2484e201f8080d6fafdeb8a2e
Juliette de Bressieux <jdb@wemanity.com>
2018-11-06 12:08:16 +0100

    Edit method roll: create an array which keeps all the scores by roll.

--- src/main/java/ressources/Game.java
+++ src/main/java/ressources/Game.java
@@ -1,9 +1,12 @@
 ackage main.java.ressources;
 
 public class Game {
+    private int rolls[]=new int[21];
+    private int currentRoll=0;
 
     // roll: called each time the player rolls the ball.
     public void roll(int numberOfPinsKnockedDown) {
+        rolls[currentRoll++]=numberOfPinsKnockedDown;
     }
 
     public int score() {
